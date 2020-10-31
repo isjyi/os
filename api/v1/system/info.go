0d3fe33d7cbe3b5a82bf0be250b240a2cc2ea79d
jerry <zhangbiao19931203@gmail.com>
2020-10-31 16:04:30 +0800

    fix

---
+++ api/v1/system/info.go
@@ -0,0 +1,8 @@
+package system
+
+import "github.com/gin-gonic/gin"
+
+func Info(c *gin.Context) {
+	// var roles = make([]string, 1)
+	// roles[0] = tools.
+}
---
+++ tools/user.go
@@ -0,0 +1,10 @@
+package tools
+
+// func ExtractClaims(c *gin.Context) jwt.MapClaims {
+// 	claims, exists := c.Get(jwt.JwtPayloadKey)
+// 	if !exists {
+// 		return make(jwt.MapClaims)
+// 	}
+
+// 	return claims.(jwt.MapClaims)
+// }
